5819b829d411aa457bffa26f0bad0501d571b8e5
Alexander Nabla Becker <becker.alexander42@gmail.com>
2016-12-10 19:51:01 +0100

    created the SPI datastructures in a global array to enable multiple SPI devices in the same chip

--- ARMSPI/armspi.c
+++ ARMSPI/armspi.c
@@ -23,41 +23,46 @@ struct ringbuffer {
 	struct packet *tail;
 };
 
+struct spi {
+	uint8_t packet rbuffer[RECIEVEBUFFERLENGHT];
+	struct packet tbuffer[TRANSMITBUFFERLENGTH];
+	struct ringbuffer recievebuffer;
+	struct ringbuffer transmitbuffer;
+	uint8_t status;
+}
+
 // The interface to the data will be the ringbuffer struct defined above, not the arrays.
-struct packet rbuffer[RECIEVEBUFFER];
-struct packet tbuffer[TRANSMITBUFFERLENGTH];
-struct ringbuffer transmitbuffer;
-struct ringbuffer recievebuffer;
+volatile struct spi[DEVICE_AMONT];
 
 // TODO this is not finished yet
-#ifdef MASTER
-void init_spi(void){
-	// init of the transmitbuffer
-	transmitbuffer.buffersize = TRANSMITBUFFERLENGTH;
-	transmitbuffer.start = tbuffer;
-	transmitbuffer.head = tbuffer;
-	transmitbuffer.tail = transmitbuffer.head+TRANSMITBUFFERLENGTH;
-
-	// init of the recievebuffer
-	recievebuffer.buffersize = RECIEVEBUFFERLENGTH;
-	recievebuffer.start = rbuffer;
-	recievebuffer.head = rbuffer;
-	recievebuffer.tail = recievebuffer.head+RECIEVEBUFFERLENGTH;
-}
-#endif
-
-#ifdef SLAVE
-void init_spi(void){
-	// init of the transmitbuffer
-	transmitbuffer.buffersize = TRANSMITBUFFERLENGTH;
-	transmitbuffer.start = tbuffer;
-	transmitbuffer.head = tbuffer;
-	transmitbuffer.tail = transmitbuffer.head+TRANSMITBUFFERLENGTH;
-
-	// init of the recievebuffer
-	recievebuffer.buffersize = RECIEVEBUFFERLENGTH;
-	recievebuffer.start = rbuffer;
-	recievebuffer.head = rbuffer;
-	recievebuffer.tail = recievebuffer.head+RECIEVEBUFFERLENGTH;
+void init_spi(uint8_t mode){
+	for(i=0;i<DEVICE_AMOUNT,i++){
+		// set up the recievebuffer pointers
+		spi[i].recievebuffer.start = spi[i].rbuffer;
+		spi[i].recievebuffer.head = spi[i].rbuffer;
+		spi[i].recievebuffer.tail = spi[i].rbuffer;
+		
+		// set up the transmitbuffer pointers
+		spi[i].transmitbuffer.start = spi[i].tbuffer;
+		spi[i].transmitbuffer.head = spi[i].tbuffer;
+		spi[i].transmitbuffer.tail = spi[i].tbuffer;
+	}
+	// Now the SPI is initialized following the instructions of the Datasheet page 698
+	if(mode == SPI_MASTER){
+		// set the baude clock rate in SPI_CR1 (BR[0:2])
+		//
+		// set the CPOL and CPHA
+		//
+		// set the Data frame format
+		//
+		// define LSB or MSB first in the SPI_CR1 register
+		//
+		// configure the NSS Bit (Slave Select)
+		//
+		// set the MSTR (for we are in master mode) bit and the SPE bit
+	}
+	else if(mode == SPI_SLAVE){
+		// TODO follow the procedure on page 697 of the manual
+	}
 }
 
