833775b286c4c462abec9ceadaeae0cc5ea3619a
Alexander Nabla Becker <becker.alexander42@gmail.com>
2016-12-10 19:51:29 +0100

    added the Mode defines

---
+++ ARMSPI/armspi.h
@@ -0,0 +1,12 @@
+/* This is the header file for the armspi library, providing an abstraction layer for the SPI interface
+ *
+ * Author: Alexander Becker
+ * Version: 0.0
+ */
+
+#define PACKETLENGTH = 4
+#define TRANSMITBUFFERLENGTH = 4
+#define RECIEVEBUFFERLENGHT = 4
+#define DEVICE_AMOUNT = 1
+#define SPI_MASTER = 0
+#define SPI_SLAVE = 1
