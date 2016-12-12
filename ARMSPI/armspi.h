6207bd33e397489ab7cf908c948530247a6daea7
Alexander Nabla Becker <becker.alexander42@gmail.com>
2016-12-13 00:22:08 +0100

    changed the script to accomodate the malloc swich

--- ARMSPI/armspi.h
+++ ARMSPI/armspi.h
@@ -4,9 +4,24 @@
  * Version: 0.0
  */
 
+#ifndef ARMSPI
+#define ARMSPI
+
+//the defines for the bits in the spi struct status byte
+#define SPIBUFFER_EMPTY		(1<<7)
+#define SPIBUFFER_FULL		(1<<6)
+
+
 #define PACKETLENGTH = 4
 #define TRANSMITBUFFERLENGTH = 4
 #define RECIEVEBUFFERLENGHT = 4
-#define DEVICE_AMOUNT = 1
-#define SPI_MASTER = 0
-#define SPI_SLAVE = 1
+
+//Uncomment, to activate the module
+#define SPI_MODULE_0
+#define SPI_MODULE_1
+// #define SPI_MODULE_2
+// #define SPI_MODULE_3
+// #define SPI_MODULE_4
+// #define SPI_MODULE_5
+
+#endif
