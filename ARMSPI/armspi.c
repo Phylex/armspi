4ee8bb07ebf5d72506a5c3f957ce933b86135391
Alexander Nabla Becker <becker.alexander42@gmail.com>
2016-12-24 22:45:02 +0100

    Integrated multiple slave suppert into the Library and linked all the defines from the libopencm3. Also finished up the Master init funktion exept for the RCC part

--- ARMSPI/armspi.c
+++ ARMSPI/armspi.c
@@ -7,17 +7,131 @@
  * Version: 0.0
  */
 
-#include "armspi.h"
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/spi.h>
 #include <libopencm3/stm32/nvic.h>
 #include <stdlib.h>
+#include "armspi.h"
+
+struct hardware_pin {
+	uint32_t port;
+	uint32_t pin;
+}
+
+#ifdef SPI_MODULE_0
+static struct hardware_pin connected_slaves_0[] =	{
+	{.port = SPI_0_SLAVE_0_PORT, .pin = SPI_0_SLAVE_0_PIN}
+#ifdef SPI_0_SLAVE_1_PORT
+	{.port = SPI_0_SLAVE_1_PORT, .pin = SPI_0_SLAVE_1_PIN}
+#endif
+#ifdef SPI_0_SLAVE_2_PORT
+	{.port = SPI_0_SLAVE_2_PORT, .pin = SPI_0_SLAVE_2_PIN}
+#endif
+#ifdef SPI_0_SLAVE_3_PORT
+	{.port = SPI_0_SLAVE_3_PORT, .pin = SPI_0_SLAVE_3_PIN}
+#endif
+#ifdef SPI_0_SLAVE_4_PORT
+	{.port = SPI_0_SLAVE_4_PORT, .pin = SPI_0_SLAVE_4_PIN}
+#endif
+}
+#endif
+
+#ifdef SPI_MODULE_1
+static struct hardware_pin connected_slaves_1[] =	{
+	{.port = SPI_1_SLAVE_0_PORT, .pin = SPI_0_SLAVE_0_PIN}
+#ifdef SPI_1_SLAVE_1_PORT
+	{.port = SPI_1_SLAVE_1_PORT, .pin = SPI_1_SLAVE_1_PIN}
+#endif
+#ifdef SPI_1_SLAVE_2_PORT
+	{.port = SPI_1_SLAVE_2_PORT, .pin = SPI_1_SLAVE_2_PIN}
+#endif
+#ifdef SPI_1_SLAVE_3_PORT
+	{.port = SPI_1_SLAVE_3_PORT, .pin = SPI_1_SLAVE_3_PIN}
+#endif
+#ifdef SPI_1_SLAVE_4_PORT
+	{.port = SPI_1_SLAVE_4_PORT, .pin = SPI_1_SLAVE_4_PIN}
+#endif
+}
+#endif
+
+#ifdef SPI_MODULE_2
+static struct hardware_pin connected_slaves_2[] =	{
+	{.port = SPI_2_SLAVE_0_PORT, .pin = SPI_2_SLAVE_0_PIN}
+#ifdef SPI_2_SLAVE_1_PORT
+	{.port = SPI_2_SLAVE_1_PORT, .pin = SPI_2_SLAVE_1_PIN}
+#endif
+#ifdef SPI_2_SLAVE_2_PORT
+	{.port = SPI_2_SLAVE_2_PORT, .pin = SPI_2_SLAVE_2_PIN}
+#endif
+#ifdef SPI_2_SLAVE_3_PORT
+	{.port = SPI_2_SLAVE_3_PORT, .pin = SPI_2_SLAVE_3_PIN}
+#endif
+#ifdef SPI_2_SLAVE_4_PORT
+	{.port = SPI_2_SLAVE_4_PORT, .pin = SPI_2_SLAVE_4_PIN}
+#endif
+}
+#endif
+
+#ifdef SPI_MODULE_3
+static struct hardware_pin connected_slaves_3[] =	{
+	{.port = SPI_3_SLAVE_0_PORT, .pin = SPI_3_SLAVE_0_PIN}
+#ifdef SPI_3_SLAVE_1_PORT
+	{.port = SPI_3_SLAVE_1_PORT, .pin = SPI_3_SLAVE_1_PIN}
+#endif
+#ifdef SPI_3_SLAVE_2_PORT
+	{.port = SPI_3_SLAVE_2_PORT, .pin = SPI_3_SLAVE_2_PIN}
+#endif
+#ifdef SPI_3_SLAVE_3_PORT
+	{.port = SPI_3_SLAVE_3_PORT, .pin = SPI_3_SLAVE_3_PIN}
+#endif
+#ifdef SPI_3_SLAVE_4_PORT
+	{.port = SPI_3_SLAVE_4_PORT, .pin = SPI_3_SLAVE_4_PIN}
+#endif
+}
+#endif
+
+#ifdef SPI_MODULE_4
+static struct hardware_pin connected_slaves_4[] =	{
+	{.port = SPI_4_SLAVE_0_PORT, .pin = SPI_4_SLAVE_0_PIN}
+#ifdef SPI_4_SLAVE_1_PORT
+	{.port = SPI_4_SLAVE_1_PORT, .pin = SPI_4_SLAVE_1_PIN}
+#endif
+#ifdef SPI_4_SLAVE_2_PORT
+	{.port = SPI_4_SLAVE_2_PORT, .pin = SPI_4_SLAVE_2_PIN}
+#endif
+#ifdef SPI_4_SLAVE_3_PORT
+	{.port = SPI_4_SLAVE_3_PORT, .pin = SPI_4_SLAVE_3_PIN}
+#endif
+#ifdef SPI_4_SLAVE_4_PORT
+	{.port = SPI_4_SLAVE_4_PORT, .pin = SPI_4_SLAVE_4_PIN}
+#endif
+}
+#endif
+
+#ifdef SPI_MODULE_5
+static struct hardware_pin connected_slaves_5[] =	{
+	{.port = SPI_5_SLAVE_0_PORT, .pin = SPI_5_SLAVE_0_PIN}
+#ifdef SPI_5_SLAVE_1_PORT
+	{.port = SPI_5_SLAVE_1_PORT, .pin = SPI_5_SLAVE_1_PIN}
+#endif
+#ifdef SPI_5_SLAVE_2_PORT
+	{.port = SPI_5_SLAVE_2_PORT, .pin = SPI_5_SLAVE_2_PIN}
+#endif
+#ifdef SPI_5_SLAVE_3_PORT
+	{.port = SPI_5_SLAVE_3_PORT, .pin = SPI_5_SLAVE_3_PIN}
+#endif
+#ifdef SPI_5_SLAVE_4_PORT
+	{.port = SPI_5_SLAVE_4_PORT, .pin = SPI_5_SLAVE_4_PIN}
+#endif
+}
+#endif
 
 struct packet {
 	uint8_t contents[PACKETLENGTH];
 	uint8_t writeindex;
 	uint8_t readindex;
+	uint8_t slave;
 };
 
 struct ringbuffer {
@@ -33,6 +147,8 @@ struct spi {
 	struct ringbuffer rxbuffer;
 	struct ringbuffer txbuffer;
 	uint8_t status;
+	uint32_t spi_hardware;
+	struct hardware_pin *connected_slaves;
 }
 
 // if the module is activated the space for the buffers is allocated
@@ -50,6 +166,10 @@ spi_module_0.rxbuffer.start = spi_module_0.rbuffer;
 spi_module_0.rxbuffer.head = spi_module_0.rbuffer;
 spi_module_0.rxbuffer.tail = spi_module_0.rbuffer;
 spi_module_0.rxbuffer.buffersize = RECIEVEBUFFERLENGHT;
+// set the corresponding hardware address
+spi_module_0.spi_hardware = SPI1
+// link the slave select pin arrays via a pointer
+spi_module_0.connected_slaves = connected_slaves_0;
 #endif
 
 #ifdef SPI_MODULE_1
@@ -64,6 +184,10 @@ spi_module_1.rxbuffer.start = spi_module_1.rbuffer;
 spi_module_1.rxbuffer.head = spi_module_1.rbuffer;
 spi_module_1.rxbuffer.tail = spi_module_1.rbuffer;
 spi_module_1.rxbuffer.buffersize = RECIEVEBUFFERLENGHT;
+// set the corresponding hardware address
+spi_module_1.spi_hardware = SPI2
+// link the slave select pin arrays via a pointer
+spi_module_1.connected_slaves = connected_slaves_1;
 #endif
 
 #ifdef SPI_MODULE_2
@@ -78,6 +202,10 @@ spi_module_2.rxbuffer.start = spi_module_2.rbuffer;
 spi_module_2.rxbuffer.head = spi_module_2.rbuffer;
 spi_module_2.rxbuffer.tail = spi_module_2.rbuffer;
 spi_module_2.rxbuffer.buffersize = RECIEVEBUFFERLENGHT;
+// set the corresponding hardware address
+spi_module_2.spi_hardware = SPI3
+// link the slave select pin arrays via a pointer
+spi_module_2.connected_slaves = connected_slaves_2;
 #endif
 
 #ifdef SPI_MODULE_3
@@ -92,6 +220,10 @@ spi_module_3.rxbuffer.start = spi_module_3.rbuffer;
 spi_module_3.rxbuffer.head = spi_module_3.rbuffer;
 spi_module_3.rxbuffer.tail = spi_module_3.rbuffer;
 spi_module_3.rxbuffer.buffersize = RECIEVEBUFFERLENGHT;
+// set the corresponding hardware address
+spi_module_3.spi_hardware = SPI4
+// link the slave select pin arrays via a pointer
+spi_module_3.connected_slaves = connected_slaves_3;
 #endif
 
 #ifdef SPI_MODULE_4
@@ -106,6 +238,10 @@ spi_module_4.rxbuffer.start = spi_module_4.rbuffer;
 spi_module_4.rxbuffer.head = spi_module_4.rbuffer;
 spi_module_4.rxbuffer.tail = spi_module_4.rbuffer;
 spi_module_4.rxbuffer.buffersize = RECIEVEBUFFERLENGHT;
+// set the corresponding hardware address
+spi_module_4.spi_hardware = SPI5
+// link the slave select pin arrays via a pointer
+spi_module_4.connected_slaves = connected_slaves_4;
 #endif
 
 #ifdef SPI_MODULE_5
@@ -120,70 +256,151 @@ spi_module_5.rxbuffer.start = spi_module_5.rbuffer;
 spi_module_5.rxbuffer.head = spi_module_5.rbuffer;
 spi_module_5.rxbuffer.tail = spi_module_5.rbuffer;
 spi_module_5.rxbuffer.buffersize = RECIEVEBUFFERLENGHT;
+// set the corresponding hardware address
+spi_module_5.spi_hardware = SPI6
+// link the slave select pin arrays via a pointer
+spi_module_5.connected_slaves = connected_slaves_5;
 #endif
 
 // TODO this is not finished yet
-void init_spi(uint8_t mode, uint8_t spi_module){
-	// set up the Hardware module
+void init_spi(uint8_t mode, struct spi *spi_module){
+	// set up the Hardware modules and pins
 	if(mode == SPI_MASTER){
 		#ifdef SPI_MODULE_0
-		spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
+		// Configuring the Hardware Interface
+		// MISO
+		gpio_set_mode(SPI_0_MISO_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, SPI_0_MISO_PIN);
+		// MOSI
+		gpio_set_mode(SPI_0_MOSI_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_0_MOSI_PIN);
+		// SCK
+		gpio_set_mode(SPI_0_SCK_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_0_SCK_PIN);
+		// Slaves
+		for(uint8_t i = 0, i < SPI_0_CONNECTED_SLAVES; i++){
+			gpio_set_mode(spi_module_0.connected_slaves[i].port, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_PUSHPULL, spi_module_0.connected_slaves[i].pin);
+		}
+		// Hardware modules
+		spi_init_master(spi_module_0.spi_hardware, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
 				SPI_CR1_CPHA_CLK_TRANSITION_2, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
-		spi_enable_tx_buffer_empty(SPI1);
-		spi_enable(SPI0);
+		spi_disable_crc(spi_module_0.spi_hardware);
+		spi_enable_rx_not_empty_interrupt(spi_module_0.spi_hardware);
+		spi_enable_software_slave_management(spi_module_0.spi_hardware);
+		spi_enable(spi_module_0.spi_hardware);
 		#endif
 
 		#ifdef SPI_MODULE_1
-		spi_init_master(SPI2, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
+		// Configuring the Hardware Interface
+		// MISO
+		gpio_set_mode(SPI_1_MISO_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, SPI_1_MISO_PIN);
+		// MOSI
+		gpio_set_mode(SPI_1_MOSI_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_1_MOSI_PIN);
+		// SCK
+		gpio_set_mode(SPI_1_SCK_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_1_SCK_PIN);
+		// Slaves
+		for(uint8_t i=0, i < SPI_1_CONNECTED_SLAVES; i++){
+			gpio_set_mode(spi_module_0.connected_slaves[i].port, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_PUSHPULL, spi_module_1.connected_slaves[i].pin);
+		}
+		spi_init_master(spi_module_1.spi_hardware, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
 				SPI_CR1_CPHA_CLK_TRANSITION_2, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
-		spi_enable_tx_buffer_empty(SPI2);
-		spi_enable(SPI1);
+		spi_disable_crc(spi_module_1.spi_hardware)
+		spi_enable_rx_not_empty_interrupt(spi_module_1.spi_hardware);
+		spi_enable_software_slave_management(spi_module_1.spi_hardware);
+		spi_enable(spi_module_1.spi_hardware);
 		#endif
 
 		#ifdef SPI_MODULE_2
-		spi_init_master(SPI3, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
+		// Configuring the Hardware Interface
+		// MISO
+		gpio_set_mode(SPI_2_MISO_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, SPI_2_MISO_PIN);
+		// MOSI
+		gpio_set_mode(SPI_2_MOSI_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_2_MOSI_PIN);
+		// SCK
+		gpio_set_mode(SPI_2_SCK_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_2_SCK_PIN);
+		// Slaves
+		for(uint8_t i=0, i < SPI_2_CONNECTED_SLAVES; i++){
+			gpio_set_mode(spi_module_2.connected_slaves[i].port, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_PUSHPULL, spi_module_2.connected_slaves[i].pin);
+		}
+		spi_init_master(spi_module_2.spi_hardware, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
 				SPI_CR1_CPHA_CLK_TRANSITION_2, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
-		spi_enable_tx_buffer_empty(SPI3);
-		spi_enable(SPI2);
+		spi_disable_crc(spi_module_2.spi_hardware)
+		spi_enable_rx_not_empty_interrupt(spi_module_2.spi_hardware);
+		spi_enable_software_slave_management(spi_module_2.spi_hardware);
+		spi_enable(spi_module_2.spi_hardware);
 		#endif
 
 		#ifdef SPI_MODULE_3
-		spi_init_master(SPI4, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
+		// Configuring the Hardware Interface
+		// MISO
+		gpio_set_mode(SPI_3_MISO_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, SPI_3_MISO_PIN);
+		// MOSI
+		gpio_set_mode(SPI_3_MOSI_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_3_MOSI_PIN);
+		// SCK
+		gpio_set_mode(SPI_3_SCK_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_3_SCK_PIN);
+		// Slaves
+		for(uint8_t i=0, i < SPI_0_CONNECTED_SLAVES; i++){
+			gpio_set_mode(spi_module_3.connected_slaves[i].port, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_PUSHPULL, spi_module_3.connected_slaves[i].pin);
+		}
+		spi_init_master(spi_module_3.spi_hardware, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
 				SPI_CR1_CPHA_CLK_TRANSITION_2, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
-		spi_enable_tx_buffer_empty(SPI4);
-		spi_enable(SPI4);
+		spi_disable_crc(spi_module_3.spi_hardware)
+		spi_enable_rx_not_empty_interrupt(spi_module_3.spi_hardware);
+		spi_enable_software_slave_management(spi_module_3.spi_hardware);
+		spi_enable(spi_module_3.spi_hardware);
 		#endif
 
 		#ifdef SPI_MODULE_4
-		spi_init_master(SPI5, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
+		// Configuring the Hardware Interface
+		// MISO
+		gpio_set_mode(SPI_4_MISO_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, SPI_4_MISO_PIN);
+		// MOSI
+		gpio_set_mode(SPI_4_MOSI_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_4_MOSI_PIN);
+		// SCK
+		gpio_set_mode(SPI_4_SCK_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_4_SCK_PIN);
+		// Slaves
+		for(uint8_t i=0, i < SPI_4_CONNECTED_SLAVES; i++){
+			gpio_set_mode(spi_module_4.connected_slaves[i].port, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_PUSHPULL, spi_module_4.connected_slaves[i].pin);
+		}
+		spi_init_master(spi_module_4.spi_hardware, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
 				SPI_CR1_CPHA_CLK_TRANSITION_2, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
-		spi_enable_tx_buffer_empty(SPI5);
-		spi_enable(SPI4);
+		spi_disable_crc(spi_module_4.spi_hardware)
+		spi_enable_rx_not_empty_interrupt(spi_module_4.spi_hardware);
+		spi_enable_software_slave_management(spi_module_4.spi_hardware);
+		spi_enable(spi_module_4.spi_hardware);
 		#endif
 
 		#ifdef SPI_MODULE_5
-		spi_init_master(SPI6, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
+		// Configuring the Hardware Interface
+		// MISO
+		gpio_set_mode(SPI_5_MISO_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, SPI_5_MISO_PIN);
+		// MOSI
+		gpio_set_mode(SPI_5_MOSI_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_5_MOSI_PIN);
+		// SCK
+		gpio_set_mode(SPI_5_SCK_PORT, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, SPI_5_SCK_PIN);
+		// Slaves
+		for(uint8_t i=0, i < SPI_5_CONNECTED_SLAVES; i++){
+			gpio_set_mode(spi_module_5.connected_slaves[i].port, GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_OUTPUT_PUSHPULL, spi_module_5.connected_slaves[i].pin);
+		}
+		spi_init_master(spi_module_5.spi_hardware, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE
 				SPI_CR1_CPHA_CLK_TRANSITION_2, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
-		spi_enable_tx_buffer_empty(SPI6);
-		spi_enable(SPI5);
+		spi_disable_crc(spi_module_5.spi_hardware)
+		spi_enable_rx_not_empty_interrupt(spi_module_5.spi_hardware);
+		spi_enable_software_slave_management(spi_module_5.spi_hardware);
+		spi_enable(spi_module_5.spi_hardware);
 		#endif
 	}
-	else if(mode == SPI_SLAVE){
+	else{
 		// TODO follow the procedure on page 697 of the manual
 	#ifdef SPI_MODULE_0
-
 	}
 }
 
 // helper funktions -----------------------------------------------------------
-void set_SS_pin_high(){
-// TODO still need to implement
+void set_SS_pin_high(struct hardware_pin ssline){
+	gpio_set(ssline.port, ssline.pin);
 }
 
-void set_SS_pin_low(){
-// TODO still need to implement
+void set_SS_pin_low(struct hardware_pin ssline){
+	gpio_clear(ssline.port, ssline.pin);
 }
-
 
 void increment_bufferpointer(struct packet *pointer, struct ringbuffer *buffer){
 	pointer ++;
@@ -191,7 +408,6 @@ void increment_bufferpointer(struct packet *pointer, struct ringbuffer *buffer){
 		pointer = buffer.start;
 	}
 }
-
 // end helper functions -------------------------------------------------------
 
 
@@ -238,7 +454,7 @@ uint16_t spi_irq_master_handler(struct spi *spimodule, uint32_t spi, uint32_t pi
 	}
 }
 
-uint16_t spi_irq_rxdata_handler(struct spi *spimodule, uint8_t data, uint32_t spi){
+uint16_t spi_irq_slave_handler(struct spi *spimodule, uint8_t data){
 	uint8_t tmprxdata = spi_read(spi);
 	for(uint8_t i=0; i>=MAXLOOP; i++){
 		if()
--- ARMSPI/armspi.h
+++ ARMSPI/armspi.h
@@ -23,63 +23,6 @@
 #define TRANSMITBUFFERLENGTH = 4
 #define RECIEVEBUFFERLENGHT = 4
 
-// here we define the ports that the different modules operate on
-// the ports defined in libopencm3/stm32/include/gpio.h and the
-// subsequently used files shouled be used
-
-// below is an example define of a port and pin setup for SPI_0
-// #define SPI_0_MISO_PORT GPIO_BANK_SPI1_MISO
-// #define SPI_0_MISO_PIN GPIO_SPI1_MISO
-//
-// #define SPI_0_MOSI_PORT GPIO_BANK_SPI1_MOSI
-// #define SPI_0_MOSI_PIN GPIO_SPI1_MOSI
-//
-// #define SPI_0_SCK_PORT GPIO_BANK_SPI1_SCK
-// #define SPI_0_SCK_PIN GPIO_SPI1_SCK
-//
-// here we define the pins that are used as the SPI pins
-#define SPI_0_MISO_PIN
-#define SPI_0_MISO_PORT
-#define SPI_0_MOSI_PIN
-#define SPI_0_MOSI_PORT
-#define SPI_0_SCK_PIN
-#define SPI_0_SCK_PORT
-
-#define SPI_1_MISO_PIN
-#define SPI_1_MISO_PORT
-#define SPI_1_MOSI_PIN
-#define SPI_1_MOSI_PORT
-#define SPI_1_SCK_PIN
-#define SPI_1_SCK_PORT
-
-#define SPI_2_MISO_PIN
-#define SPI_2_MISO_PORT
-#define SPI_2_MOSI_PIN
-#define SPI_2_MOSI_PORT
-#define SPI_2_SCK_PIN
-#define SPI_2_SCK_PORT
-
-#define SPI_3_MISO_PIN
-#define SPI_3_MISO_PORT
-#define SPI_3_MOSI_PIN
-#define SPI_3_MOSI_PORT
-#define SPI_3_SCK_PIN
-#define SPI_3_SCK_PORT
-
-#define SPI_4_MISO_PIN
-#define SPI_4_MISO_PORT
-#define SPI_4_MOSI_PIN
-#define SPI_4_MOSI_PORT
-#define SPI_4_SCK_PIN
-#define SPI_4_SCK_PORT
-
-#define SPI_5_MISO_PIN
-#define SPI_5_MISO_PORT
-#define SPI_5_MOSI_PIN
-#define SPI_5_MOSI_PORT
-#define SPI_5_SCK_PIN
-#define SPI_5_SCK_PORT
-
 //Uncomment, to activate the module
 // #define SPI_MODULE_0
 // #define SPI_MODULE_1
@@ -87,4 +30,93 @@
 // #define SPI_MODULE_3
 // #define SPI_MODULE_4
 // #define SPI_MODULE_5
+
+// Now we define how many slaves are connected to the 
+#define SPI_0_CONNECTED_SLAVES 1
+#define SPI_1_CONNECTED_SLAVES 1
+#define SPI_2_CONNECTED_SLAVES 1
+#define SPI_3_CONNECTED_SLAVES 1
+#define SPI_4_CONNECTED_SLAVES 1
+#define SPI_5_CONNECTED_SLAVES 1
+
+// and the corresponding port and pin registers
+// the SS lines for Slave 0 are all allready set to the 
+//
+#define SPI_0_SLAVE_0_PORT GPIO_BANK_SPI1_NSS 
+#define SPI_0_SLAVE_0_PIN GPIO_SPI1_NSS
+//#define SPI_0_SLAVE_0_PORT
+//#define SPI_0_SLAVE_0_PIN
+
+#define SPI_1_SLAVE_0_PORT GPIO_BANK_SPI2_NSS
+#define SPI_1_SLAVE_0_PIN GPIO_SPI2_NSS
+//#define SPI_1_SLAVE_0_PORT
+//#define SPI_1_SLAVE_0_PIN
+
+#define SPI_2_SLAVE_0_PORT GPIO_BANK_SPI3_NSS
+#define SPI_2_SLAVE_0_PIN GPIO_SPI3_NSS
+//#define SPI_2_SLAVE_0_PORT
+//#define SPI_2_SLAVE_0_PIN
+
+#define SPI_3_SLAVE_0_PORT GPIO_BANK_SPI4_NSS  
+#define SPI_3_SLAVE_0_PIN GPIO_SPI4_NSS
+//#define SPI_3_SLAVE_0_PORT
+//#define SPI_3_SLAVE_0_PIN
+
+#define SPI_4_SLAVE_0_PORT GPIO_BANK_SPI5_NSS 
+#define SPI_4_SLAVE_0_PIN GPIO_SPI5_NSS
+//#define SPI_4_SLAVE_0_PORT
+//#define SPI_4_SLAVE_0_PIN
+
+#define SPI_5_SLAVE_0_PORT GPIO_BANK_SPI6_NSS 
+#define SPI_5_SLAVE_0_PIN GPIO_SPI6_NSS
+//#define SPI_5_SLAVE_0_PORT
+//#define SPI_5_SLAVE_0_PIN
+
+// here we define the ports that the different modules operate on
+// the ports defined in libopencm3/stm32/include/gpio.h and the
+// subsequently used files shouled be used
+
+#define SPI_0_MISO_PIN   GPIO_BANK_SPI1_MISO
+#define SPI_0_MISO_PORT  GPIO_SPI1_MISO
+#define SPI_0_MOSI_PIN   GPIO_BANK_SPI1_MOSI
+#define SPI_0_MOSI_PORT  GPIO_SPI1_MOSI
+#define SPI_0_SCK_PIN    GPIO_BANK_SPI1_SCK
+#define SPI_0_SCK_PORT   GPIO_SPI1_SCK
+
+#define SPI_1_MISO_PIN   GPIO_BANK_SPI2_MISO
+#define SPI_1_MISO_PORT  GPIO_SPI2_MISO
+#define SPI_1_MOSI_PIN   GPIO_BANK_SPI2_MOSI
+#define SPI_1_MOSI_PORT  GPIO_SPI2_MOSI
+#define SPI_1_SCK_PIN    GPIO_BANK_SPI2_SCK
+#define SPI_1_SCK_PORT   GPIO_SPI2_SCK
+
+#define SPI_2_MISO_PIN   GPIO_BANK_SPI3_MISO
+#define SPI_2_MISO_PORT  GPIO_SPI3_MISO     
+#define SPI_2_MOSI_PIN   GPIO_BANK_SPI3_MOSI
+#define SPI_2_MOSI_PORT  GPIO_SPI3_MOSI
+#define SPI_2_SCK_PIN    GPIO_BANK_SPI3_SCK
+#define SPI_2_SCK_PORT   GPIO_SPI3_SCK
+
+#define SPI_3_MISO_PIN   GPIO_BANK_SPI4_MISO
+#define SPI_3_MISO_PORT  GPIO_SPI4_MISO     
+#define SPI_3_MOSI_PIN   GPIO_BANK_SPI4_MOSI
+#define SPI_3_MOSI_PORT  GPIO_SPI4_MOSI
+#define SPI_3_SCK_PIN    GPIO_BANK_SPI4_SCK
+#define SPI_3_SCK_PORT   GPIO_SPI4_SCK
+
+#define SPI_4_MISO_PIN   GPIO_BANK_SPI5_MISO
+#define SPI_4_MISO_PORT  GPIO_SPI5_MISO     
+#define SPI_4_MOSI_PIN   GPIO_BANK_SPI5_MOSI
+#define SPI_4_MOSI_PORT  GPIO_SPI5_MOSI
+#define SPI_4_SCK_PIN    GPIO_BANK_SPI5_SCK
+#define SPI_4_SCK_PORT   GPIO_SPI5_SCK
+
+#define SPI_5_MISO_PIN   GPIO_BANK_SPI6_MISO
+#define SPI_5_MISO_PORT  GPIO_SPI6_MISO     
+#define SPI_5_MOSI_PIN   GPIO_BANK_SPI6_MOSI
+#define SPI_5_MOSI_PORT  GPIO_SPI6_MOSI
+#define SPI_5_SCK_PIN    GPIO_BANK_SPI6_SCK
+#define SPI_5_SCK_PORT   GPIO_SPI6_SCK
+
+
 #endif
