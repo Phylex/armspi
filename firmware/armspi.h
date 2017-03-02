581aeadf5548a151d2e8a3508b55e6445045eb6c
Alexander Nabla Becker <becker.alexander42@gmail.com>
2017-03-02 17:04:27 +0000

    altered the definitions to keep up with the changes of the armspi.c. Added the feature that the clock prescalers can be setls for every SPI module

--- firmware/armspi.h
+++ firmware/armspi.h
@@ -13,8 +13,10 @@
 #include <libopencm3/cm3/nvic.h>
 
 //the defines for the bits in the spi struct status byte
-#define SPIBUFFER_EMPTY		(1<<7)
-#define SPIBUFFER_FULL		(1<<6)
+#define SPITXBUFFER_EMPTY		(1<<7)
+#define SPITXBUFFER_FULL		(1<<6)
+#define SPIRXBUFFER_EMPTY		(1<<8)
+#define SPIRXBUFFER_FULL		(1<<9)
 #define SPI_MASTER		(1<<5)
 #define SPI_PACKET_RECIEVED	(1<<4)
 #define SPI_PACKET_BEING_SENT	(1<<3)
@@ -26,7 +28,7 @@
 #define MAXLOOP 10
 
 //Uncomment, to activate the module
- #define SPI_MODULE_1
+#define SPI_MODULE_1
 // #define SPI_MODULE_2
 // #define SPI_MODULE_3
 // #define SPI_MODULE_4
@@ -41,6 +43,120 @@
 #define SPI_5_MODE SPI_MASTER
 #define SPI_6_MODE SPI_MASTER
 
+//We now define the Clock rate of the SPI-Module as prescaler to the APB2 Domain for the SPI1
+// uncomment the SPI_CKL_DIV that has the correct prescaler
+// prescaler Div 2
+//#define SPI1_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_2
+// prescaler Div 4
+//#define SPI1_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_4
+// prescaler Div 8
+//#define SPI1_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_8
+// prescaler Div 16
+//#define SPI1_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_16
+// prescaler Div 32
+//#define SPI1_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_32
+// prescaler Div 64
+#define SPI1_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_64
+// prescaler Div 128
+//#define SPI1_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_128
+// prescaler Div 256
+//#define SPI1_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_256
+
+//Clock rate for SPI2
+// uncomment the SPI_CKL_DIV that has the correct prescaler
+// prescaler Div 2
+//#define SPI2_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_2
+// prescaler Div 4
+//#define SPI2_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_4
+// prescaler Div 8
+//#define SPI2_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_8
+// prescaler Div 16
+//#define SPI2_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_16
+// prescaler Div 32
+//#define SPI2_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_32
+// prescaler Div 64
+#define SPI2_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_64
+// prescaler Div 128
+//#define SPI2_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_128
+// prescaler Div 256
+//#define SPI2_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_256
+
+//Clock rate for SPI3
+// uncomment the SPI_CKL_DIV that has the correct prescaler
+// prescaler Div 2
+//#define SPI3_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_2
+// prescaler Div 4
+//#define SPI3_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_4
+// prescaler Div 8
+//#define SPI3_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_8
+// prescaler Div 16
+//#define SPI3_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_16
+// prescaler Div 32
+//#define SPI3_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_32
+// prescaler Div 64
+#define SPI3_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_64
+// prescaler Div 128
+//#define SPI3_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_128
+// prescaler Div 256
+//#define SPI3_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_256
+
+//Clock rate for SPI4
+// uncomment the SPI_CKL_DIV that has the correct prescaler
+// prescaler Div 2
+//#define SPI4_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_2
+// prescaler Div 4
+//#define SPI4_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_4
+// prescaler Div 8
+//#define SPI4_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_8
+// prescaler Div 16
+//#define SPI4_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_16
+// prescaler Div 32
+//#define SPI4_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_32
+// prescaler Div 64
+#define SPI4_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_64
+// prescaler Div 128
+//#define SPI4_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_128
+// prescaler Div 256
+//#define SPI4_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_256
+
+//Clock rate for SPI5
+// uncomment the SPI_CKL_DIV that has the correct prescaler
+// prescaler Div 2
+//#define SPI5_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_2
+// prescaler Div 4
+//#define SPI5_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_4
+// prescaler Div 8
+//#define SPI5_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_8
+// prescaler Div 16
+//#define SPI5_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_16
+// prescaler Div 32
+//#define SPI5_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_32
+// prescaler Div 64
+#define SPI5_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_64
+// prescaler Div 128
+//#define SPI5_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_128
+// prescaler Div 256
+//#define SPI5_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_256
+
+//Clock rate for SPI6
+// uncomment the SPI_CKL_DIV that has the correct prescaler
+// prescaler Div 2
+//#define SPI6_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_2
+// prescaler Div 4
+//#define SPI6_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_4
+// prescaler Div 8
+//#define SPI6_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_8
+// prescaler Div 16
+//#define SPI6_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_16
+// prescaler Div 32
+//#define SPI6_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_32
+// prescaler Div 64
+#define SPI6_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_64
+// prescaler Div 128
+//#define SPI6_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_128
+// prescaler Div 256
+//#define SPI6_CLK_DIV SPI_CR1_BAUDRATE_FPCLK_DIV_256
+
 
 // Now we define how many slaves are connected to the
 #define SPI_1_CONNECTED_SLAVES 1
@@ -54,31 +170,41 @@
 // the SS lines for Slave 0 are all allready set to the
 //
 #define SPI_1_SLAVE_0_PORT GPIO_BANK_SPI1_NSS
+#define SPI_1_SLAVE_0_RCC RCC_GPIOA
 #define SPI_1_SLAVE_0_PIN GPIO_SPI1_NSS
-//#define SPI_0_SLAVE_0_PORT
-//#define SPI_0_SLAVE_0_PIN
+//#define SPI_1_SLAVE_1_PORT
+//#define SPI_1_SLAVE_1_RCC
+//#define SPI_1_SLAVE_1_PIN
 
 #define SPI_2_SLAVE_0_PORT GPIO_BANK_SPI2_NSS
+#define SPI_2_SLAVE_0_RCC RCC_GPIOB
 #define SPI_2_SLAVE_0_PIN GPIO_SPI2_NSS
-//#define SPI_1_SLAVE_0_PORT
-//#define SPI_1_SLAVE_0_PIN
+//#define SPI_2_SLAVE_1_PORT
+//#define SPI_2_SLAVE_1_RCC
+//#define SPI_2_SLAVE_1_PIN
 
+// TODO collect the RCC registers for the other SPI_Modules in bigger chips
 #define SPI_3_SLAVE_0_PORT GPIO_BANK_SPI3_NSS
+#define SPI_3_SLAVE_0_RCC 
 #define SPI_3_SLAVE_0_PIN GPIO_SPI3_NSS
-//#define SPI_2_SLAVE_0_PORT
-//#define SPI_2_SLAVE_0_PIN
+//#define SPI_3_SLAVE_1_PORT
+//#define SPI_3_SLAVE_1_RCC
+//#define SPI_3_SLAVE_1_PIN
 
 #define SPI_4_SLAVE_0_PORT GPIO_BANK_SPI4_NSS
+#define SPI_4_SLAVE_0_RCC 
 #define SPI_4_SLAVE_0_PIN GPIO_SPI4_NSS
 //#define SPI_3_SLAVE_0_PORT
 //#define SPI_3_SLAVE_0_PIN
 
 #define SPI_5_SLAVE_0_PORT GPIO_BANK_SPI5_NSS
+#define SPI_5_SLAVE_0_RCC 
 #define SPI_5_SLAVE_0_PIN GPIO_SPI5_NSS
 //#define SPI_4_SLAVE_0_PORT
 //#define SPI_4_SLAVE_0_PIN
 
 #define SPI_6_SLAVE_0_PORT GPIO_BANK_SPI6_NSS
+#define SPI_6_SLAVE_0_RCC 
 #define SPI_6_SLAVE_0_PIN GPIO_SPI6_NSS
 //#define SPI_5_SLAVE_0_PORT
 //#define SPI_5_SLAVE_0_PIN
@@ -88,45 +214,64 @@
 // subsequently used files shouled be used
 
 #define SPI_1_MISO_PORT  GPIO_BANK_SPI1_MISO
+#define SPI_1_MISO_RCC   RCC_GPIOA
 #define SPI_1_MISO_PIN   GPIO_SPI1_MISO
 #define SPI_1_MOSI_PORT  GPIO_BANK_SPI1_MOSI
+#define SPI_1_MOSI_RCC   RCC_GPIOA
 #define SPI_1_MOSI_PIN   GPIO_SPI1_MOSI
 #define SPI_1_SCK_PORT   GPIO_BANK_SPI1_SCK
+#define SPI_1_SCK_RCC    RCC_GPIOA
 #define SPI_1_SCK_PIN    GPIO_SPI1_SCK
 
 #define SPI_2_MISO_PORT  GPIO_BANK_SPI2_MISO
+#define SPI_2_MISO_RCC   RCC_GPIOB
 #define SPI_2_MISO_PIN   GPIO_SPI2_MISO
 #define SPI_2_MOSI_PORT  GPIO_BANK_SPI2_MOSI
+#define SPI_2_MOSI_RCC   RCC_GPIOB
 #define SPI_2_MOSI_PIN   GPIO_SPI2_MOSI
 #define SPI_2_SCK_PORT   GPIO_BANK_SPI2_SCK
+#define SPI_2_SCK_RCC    RCC_GPIOB
 #define SPI_2_SCK_PIN    GPIO_SPI2_SCK
 
+// TODO add the standard definitions for the RCC of the OUTPUT PORT
 #define SPI_3_MISO_PORT  GPIO_BANK_SPI3_MISO
+#define SPI_3_MISO_RCC
 #define SPI_3_MISO_PIN   GPIO_SPI3_MISO
 #define SPI_3_MOSI_PORT  GPIO_BANK_SPI3_MOSI
+#define SPI_3_MOSI_RCC
 #define SPI_3_MOSI_PIN   GPIO_SPI3_MOSI
 #define SPI_3_SCK_PORT   GPIO_BANK_SPI3_SCK
+#define SPI_3_SCK_RCC
 #define SPI_3_SCK_PIN    GPIO_SPI3_SCK
 
 #define SPI_4_MISO_PORT  GPIO_BANK_SPI4_MISO
+#define SPI_3_MISO_RCC
 #define SPI_4_MISO_PIN   GPIO_SPI4_MISO
 #define SPI_4_MOSI_PORT  GPIO_BANK_SPI4_MOSI
+#define SPI_3_MOSI_RCC
 #define SPI_4_MOSI_PIN   GPIO_SPI4_MOSI
 #define SPI_4_SCK_PORT   GPIO_BANK_SPI4_SCK
+#define SPI_3_SCK_RCC
 #define SPI_4_SCK_PIN    GPIO_SPI4_SCK
 
 #define SPI_5_MISO_PORT  GPIO_BANK_SPI5_MISO
+#define SPI_5_MISO_RCC
 #define SPI_5_MISO_PIN   GPIO_SPI5_MISO
 #define SPI_5_MOSI_PORT  GPIO_BANK_SPI5_MOSI
+#define SPI_5_MOSI_RCC
 #define SPI_5_MOSI_PIN   GPIO_SPI5_MOSI
 #define SPI_5_SCK_PORT   GPIO_BANK_SPI5_SCK
+#define SPI_5_SCK_RCC
 #define SPI_5_SCK_PIN    GPIO_SPI5_SCK
 
 #define SPI_6_MISO_PORT  GPIO_BANK_SPI6_MISO
+#define SPI_5_MISO_RCC
 #define SPI_6_MISO_PIN   GPIO_SPI6_MISO
 #define SPI_6_MOSI_PORT  GPIO_BANK_SPI6_MOSI
+#define SPI_5_MOSI_RCC
 #define SPI_6_MOSI_PIN   GPIO_SPI6_MOSI
 #define SPI_6_SCK_PORT   GPIO_BANK_SPI6_SCK
+#define SPI_5_SCK_RCC
 #define SPI_6_SCK_PIN    GPIO_SPI6_SCK
 
 struct hardware_pin;
@@ -150,14 +295,14 @@ struct spi {
 	struct packet tbuffer[TRANSMITBUFFERLENGTH];
 	volatile struct ringbuffer rxbuffer;
 	volatile struct ringbuffer txbuffer;
-	uint8_t status;
+	uint16_t status;
 	uint32_t spi_hardware;
 	const struct hardware_pin *connected_slaves;
 };
 
 
 void init_spi(void);
-uint8_t write_spi_packet(volatile struct spi *spimodule, uint8_t *data, uint8_t size);
+uint8_t write_spi_packet(volatile struct spi *spimodule, uint8_t *data, uint8_t size,uint8_t slave_nr);
 uint8_t read_spi_packet(volatile struct spi *spimodule, uint8_t *data, uint8_t size);
 
 #endif
